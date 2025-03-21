3d3f59f3bb33385fd47014188c395e6efaeadecc
Alessio Frittoli <info@alessiofrittoli.it>
2025-03-21 16:01:59 +0100

    tests: improved tests scripts

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,7 @@
 import type { JestConfigWithTsJest } from 'ts-jest'
 import dotenv from 'dotenv'
 
-const env		= process.env.NODE_ENV
-const testEnv	= process.env.JSDOM === 'true' ? 'jest-environment-jsdom' : 'node'
+const env = process.env.NODE_ENV
 
 dotenv.config( { path: [
 	`.env.${ env }.local`,
@@ -18,7 +17,7 @@ dotenv.config( { path: [
  */
 const config: JestConfigWithTsJest = {
 	/** https://jestjs.io/docs/configuration#testenvironment-string */
-	testEnvironment: testEnv,
+	testEnvironment: 'node',
 	moduleDirectories: [ 'node_modules', '<rootDir>/' ],
 	setupFilesAfterEnv: [ './jest.setup.ts' ],
 	/**
