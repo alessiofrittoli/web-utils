bf4704e823b9e9ea0b68e6858335d142d6e4f4f7
Alessio Frittoli <info@alessiofrittoli.it>
2025-04-23 13:13:14 +0200

    feat: add `isPortrait` utility function

---
+++ src/device/index.ts
@@ -0,0 +1,15 @@
+import { getMediaMatches } from '@/browser-api'
+
+/**
+ * Use this media query to check if document currently matches the portrait device orientation.
+ * 
+ */
+export const portraitMediaQuery = '(orientation:portrait)'
+
+
+/**
+ * Check if device is in portrait orientation.
+ *
+ * @returns `true` if the device is in portrait orientation when this function is executed, `false` otherwise.
+ */
+export const isPortrait = () => getMediaMatches( portraitMediaQuery )
