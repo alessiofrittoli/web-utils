9a6cc6d9654316470acfad138924a3c7aa34218d
Alessio Frittoli <info@alessiofrittoli.it>
2025-07-14 14:52:32 +0200

    feat: add `deferTask` utility function

--- src/promises.ts
+++ src/promises.ts
@@ -5,3 +5,29 @@
  * @returns	A new Promise which get resolved after the specified time.
  */
 export const sleep = ( time: number ) => new Promise<void>( resolve => setTimeout( resolve, time ) )
+
+
+/**
+ * Defer task so main-thread is not blocked in order to quickly paint and respond to user interaction.
+ *
+ * @param task The task callable function.
+ * 
+ * @returns A new Promise which returns the `task` result once fulfilled.
+ */
+export const deferTask = <T extends () => unknown | Promise<unknown>>( task: T ): Promise<Awaited<ReturnType<T>>> => (
+	new Promise<Awaited<ReturnType<T>>>( ( resolve, reject ) => {
+		const tick = () => {
+			setTimeout( async () => {
+				try {
+					resolve( ( await task() ) as Awaited<ReturnType<T>> )
+				} catch ( error ) {
+					reject( error )
+				}
+			}, 0 )
+		}		
+
+		if ( typeof requestAnimationFrame !== 'function' ) return tick()
+
+		requestAnimationFrame( tick )
+	} )
+)
