67c4112f1fbba2ae9f0f8ffe8710bb689886a1ab
Alessio Frittoli <info@alessiofrittoli.it>
2025-07-14 14:52:42 +0200

    tests: add `deferTask` unit tests

---
+++ __tests__/promises.jsdom.test.ts
@@ -0,0 +1,105 @@
+/**
+ * @jest-environment jsdom
+ */
+import { deferTask } from '@/promises'
+
+
+describe( 'deferTask', () => {
+
+	let rafSpy: jest.SpyInstance<number, [ FrameRequestCallback ]>
+
+	beforeEach( () => {
+
+		jest.useFakeTimers()
+		rafSpy = (
+			jest.spyOn( window, 'requestAnimationFrame' )
+				.mockImplementation( cb => {
+					cb( performance.now() )
+					return 1
+				} )
+		)
+
+	} )
+
+
+	afterEach( () => {
+
+		rafSpy.mockRestore()
+		jest.useRealTimers()
+
+	} )
+
+
+	it( 'resolves with the result of a synchronous task', async () => {
+
+		const task		= jest.fn( () => 42 )
+		const promise	= deferTask( task )
+
+		jest.runAllTimers()
+		
+		await expect( promise ).resolves.toBe( 42 )
+		
+		expect( task ).toHaveBeenCalled()
+
+	} )
+	
+
+	it( 'resolves with the result of an asynchronous task', async () => {
+
+		const task		= jest.fn( async () => 'async result' )
+		const promise	= deferTask( task )
+
+		jest.runAllTimers()
+
+		await expect( promise ).resolves.toBe( 'async result' )
+		expect( task ).toHaveBeenCalled()
+
+	} )
+
+
+	it( 'rejects if the task throws synchronously', async () => {
+
+		const error		= new Error( 'fail' )
+		const task		= jest.fn( () => { throw error } )
+		const promise	= deferTask( task )
+
+		jest.runAllTimers()
+
+		await expect( promise ).rejects.toBe( error )
+		expect( task ).toHaveBeenCalled()
+
+	} )
+
+
+	it( 'rejects if the task returns a rejected promise', async () => {
+
+		const error		= new Error( 'async fail' )
+		const task		= jest.fn( async () => { throw error })
+		const promise	= deferTask( task )
+
+		jest.runAllTimers()
+
+		await expect( promise ).rejects.toBe( error )
+		expect( task ).toHaveBeenCalled()
+
+	} )
+
+
+	it( 'defers task with requestAnimationFrame and setTimeout', async () => {
+
+		const task			= jest.fn( () => 1 )
+		const setTimeoutSpy	= jest.spyOn( window, 'setTimeout' )
+		const promise		= deferTask( task )
+
+		jest.runAllTimers()
+
+		await promise
+
+		expect( rafSpy ).toHaveBeenCalled()
+		expect( setTimeoutSpy ).toHaveBeenCalledWith( expect.any( Function ), 0 )
+
+		setTimeoutSpy.mockRestore()
+
+	} )
+
+} )
