e4ee36a3c42510e170fb0026bae7e3976d9c812e
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2025-07-15 19:31:22 +0200

    feature/deferCallback-utility-function (#56)

--- src/promises.ts
+++ src/promises.ts
@@ -19,12 +19,25 @@ export const sleep = ( time: number ) => new Promise<void>( resolve => setTimeou
  * @param args Arguments required by the given `task` function.
  * 
  * @returns A new Promise which returns the `task` result once fulfilled.
+ * 
+ * @example
+ *
+ * ```ts
+ * const longTask = ( event: Event ) => {
+ * 	...
+ * }
+ *
+ * button.addEventListener( 'click', event => {
+ * 	deferTask( longTask, event )
+ * } )
+ * ```
  */
 export const deferTask = <
 	T extends ( ...args: U ) => unknown | Promise<unknown>,
 	U extends unknown[]
 >( task: T, ...args: U ): Promise<Awaited<ReturnType<T>>> => (
 	new Promise<Awaited<ReturnType<T>>>( ( resolve, reject ) => {
+
 		const tick = () => {
 			setTimeout( async () => {
 				try {
@@ -35,8 +48,42 @@ export const deferTask = <
 			}, 0 )
 		}		
 
-		if ( typeof requestAnimationFrame !== 'function' ) return tick()
+		if ( typeof requestAnimationFrame !== 'function' ) {
+			return tick()
+		}
 
 		requestAnimationFrame( tick )
+
 	} )
+)
+
+
+/**
+ * Defer task handler so main-thread is not blocked in order to quickly paint and respond to user interaction.
+ * 
+ * It esponentially decrease INP process timings.
+ * 
+ * @template T The task function definition.
+ * @template U The task function arguments.
+ * 
+ * @param task The task callable function.
+ * @param args Arguments required by the given `task` function.
+ * 
+ * @returns A new handler which returns a new Promise that returns the `task` result once fulfilled.
+ * 
+ * @example
+ *
+ * ```ts
+ * const longTask = ( event: Event ) => {
+ * 	...
+ * }
+ *
+ * button.addEventListener( 'click', deferCallback( longTask ) )
+ * ```
+ */
+export const deferCallback = <
+	T extends ( ...args: U ) => unknown | Promise<unknown>,
+	U extends unknown[],
+>( task: T ): ( ...args: U ) => Promise<Awaited<ReturnType<T>>> => (
+	( ...args: U ) => deferTask( task, ...args )
 )
