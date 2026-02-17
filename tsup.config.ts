23832c82884e3676dbcf64b6b7d860b43b41e7f4
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2026-02-17 20:47:12 +0100

    feat: removed export paths and reduced bundle size (#68)

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from 'tsup'
 const isProduction = process.env.NODE_ENV === 'production'
 
 export default defineConfig( {
-	entry		: [ 'src/**/*.ts' ],
+	entry		: [ 'src/**/index.ts' ],
 	format		: [ 'cjs', 'esm' ],
 	dts			: true,
-	splitting	: false,
+	splitting	: true,
 	shims		: true,
 	skipNodeModulesBundle: true,
 	clean		: true,
